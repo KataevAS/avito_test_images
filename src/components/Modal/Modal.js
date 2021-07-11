7d506b98cb78da429a20ccca0f794980d7421b3f
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-07-11 13:03:44 +0300

    add Portal for Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { createPortal } from 'react-dom';
 import Comment from '../Comment/Comment';
 import style from './style.module.css'
 
 
+const modalRoot = document.getElementById('modal-root');
 
 const Modal = ({ comments, url, alt = 'image', closeModal }) => {
 
-    return (
+    return createPortal((
         <div className={style.wrapper}>
             <div className={style.modal}>
                 <button className={style.closeBtn} onClick={closeModal}>X</button>
@@ -18,7 +20,7 @@ const Modal = ({ comments, url, alt = 'image', closeModal }) => {
                 </div>
             </div>
         </div>
-    )
+    ), modalRoot)
 }
 
 export default Modal;
